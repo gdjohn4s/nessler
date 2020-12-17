c369e33add43f9a65e42b1ee23fa57cc25938003
gdjohn4s <john4s@protonmail.ch>
2020-12-18 00:56:13 +0100

    added nessler.js

---
+++ server/components/nessler/nessler.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const monk = require('monk');
+
+require('dotenv').config();
+
+// - Mongo Configuration
+// - It will connect to the mongo database and find nessler collection, if not found, it will created
+const db = monk(process.env.MONGO_URI);
+const nessler = db.get('nessler')
+
+// - Nessler middleware
+// - middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+    console.log('Requested Nessler URI Path : ', req.url)
+    next()
+  }) 
+
+// - Get all nesslers with a db find function
+router.get('/nessler', (req, res, next) => {
+    nessler
+        .find()
+        .then(nessler => {
+            res.json(nessler);
+        }).catch(next);
+});
+
+// - Validation function
+function isValidNessler(nessler) {
+    return nessler.user && nessler.user.toString().trim() !== '' && nessler.user.toString().trim().length <= 20 && 
+    nessler.nessler && nessler.nessler.toString().trim() !== '' && nessler.nessler.toString().trim().length <= 100 ;
+}
+
+// - Insert a nessler into the database
+router.post('/addNessler', (req, res, err) => {
+    if (isValidNessler(req.body)) {
+        const newNessler = {
+            user: req.body.user,
+            nessler: req.body.nessler,
+            created: new Date()
+        };
+
+        nessler
+            .insert(nessler)
+            .then(createdNessler => {
+                res.json(createdNessler);
+            }).catch(err);
+    } else {
+        res.status(422);
+        res.json({
+            httpStatus: res.statusCode,
+            message: "User and Nessler are required! Name cannot be longer than 20 chars and nessler cannot be longer than 100 chars."
+        });
+    }
+});
+
+module.exports = router;
