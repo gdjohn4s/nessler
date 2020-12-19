8a94038120d0c81a129f25d17f0d5d254c070866
gdjohn4s <john4s@protonmail.ch>
2020-12-19 03:50:43 +0100

    updated index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const router = express.Router();
 const user = require('./user.js');
 const testApi = require('./test/testApi.js');
 const nesslerApi = require('./components/nessler/nessler.js');
@@ -11,23 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/v1/', user, testApi, nesslerApi);
-
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: "Welcome"
-//     })
-// })
-
-app.post('/geppetto', (req, res) => {
-    const cacca = {
-        cacca: req.body.cacca
-    };
-
-    res.json({
-        content: cacca,
-        statusCode: res.statusCode
-    });
-});
 
 app.listen(3000, (err) => {
     console.log("Listening");
