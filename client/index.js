ecb23ac067bf46fa6c0b54555053e08c22f9bf12
gdjohn4s <john4s@protonmail.ch>
2020-12-23 01:23:49 +0100

    rewrited fetch function

--- client/index.js
+++ client/index.js
@@ -1,4 +1,9 @@
-function insertNessler() {
+const container = document.querySelector('.container');
+const form = document.querySelector('form'); // grabbing an element on the page
+const API_URL = "http://localhost:3000/v1/addNessler/";
+
+form.addEventListener('submit', (event) => {
+    console.log('form was clicked');
     var nameIn = document.querySelector('input').value;
     var message = document.querySelector('textarea').value;
 
@@ -7,21 +12,25 @@ function insertNessler() {
         nessler: message
     }
 
-    schema = JSON.stringify(schema);
-
-    fetch("http://localhost:3000/v1/addNessler/", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: schema
-        })
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function (data) {
-            console.log(JSON.stringify(data))
-        })
-    console.log(schema);
-}
+    if (nameIn && message){
+        schema = JSON.stringify(schema);
+    
+        fetch(API_URL, {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: schema
+            })
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (data) {
+                console.log(JSON.stringify(data))
+            })
+        console.log(schema);
+    } else {
+        alert('Please insert the name or the message! Thanks')
+    }
+});
